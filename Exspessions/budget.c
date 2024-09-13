1ed2c5222340a0b76bb15e09664146a94dadd445
coolguyben <benjamin.robison@ucas-edu.net>
2024-09-13 19:16:11 +0000

    budget.c

--- Exspessions/budget.c
+++ Exspessions/budget.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
 int main(void){
-float income[9];
-char utilities[9];
-char grocies[9];
-char car[9];
+float income, rent, utilities, grociries, transportation, saving, spend;
+float prent, putilities, pgroceries, p
 printf("This is a budget calculator. /n how much do you make a month?/n");
 scanf(income, sizeof(income), stdin);
-printf("how much do your utilities/n");
-scanf("%s", utilities);
-scanf("%1f" , income);
-printf("you mske %lf", income);
-float spend[] = (float) income - (float) utilities;
-printf("%f",spend);
+printf("%f", &utilities);
+scanf("%f", utilities);
+printf("%f" , inco);
+printf("you make %f", &income);
+scanf("%f", &transportation);
+savings = income * .2;
+expenses = rent + utilities +grocieries + transportation;
+ = income - expenses - savings;
+printf("you make %.2f/n",income");
+printf("your exspenses are $%.2/n",spend)
+printf
+printf
+prent = rent/ income *100;
+putilities = utilities/ income *100;
+ptransportation = transportation/ income *100;
+psavings = savings/income *100;
+printf("your rent is %d%% of your income./n",(int)prent);
+printf("your utilities are %d%% of your income./n",(int)prent);
 return 0;
 }
