0209a718cf9746e23d14036e7f1761374f4f3aec
coolguyben <benjamin.robison@ucas-edu.net>
2024-10-04 19:43:24 +0000

    pryor

---
+++ Exspessions/calculator.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+float income, rent, utilities, grocieries, transportation,savings,expenses,spend;
+float prent, putilities, pgrocieries, ptransportation, psavings, pexpenses;
+
+int main(void){
+printf("this is a budget calculator.\n How much do you make a month\n");
+scanf("%f", &income); 
+printf(" How much does your rent cost\n ");
+scanf("%f", &rent);
+printf(" How much does your utilities cost\n ");
+scanf("%f", &utilities);
+printf(" How much do your groceries cost\n ");
+scanf("%f", &grocieries);
+printf(" How much does your transportation cost\n ");
+scanf("%f", &transportation); 
+savings = income* .2;
+expenses = rent + utilities + grocieries + transportation;
+spend = income - expenses - savings;
+printf ("you make %.2f\n", income);
+printf ("you spend%.2f\n", expenses);
+printf ("you save %.2f\n", savings);
+printf ("your saving money is %.2f\n", spend);
+prent = rent/income*100;
+putilities= utilities/income*100;
+pgrocieries= grocieries/income*100;
+ptransportation= transportation/income*100;
+psavings= savings/income*100;
+pexpenses= expenses/income*100;
+printf("your rent is %d%% of your income.\n", (int)prent);
+printf("your utilities is %d%% of your income.\n", (int)putilities);
+printf("your grocieries are %d%% of your income.\n", (int)pgrocieries);
+printf("your transpotation is %d%% of your income.\n", (int)ptransportation);
+printf("your savings are %d%% of your income.\n", (int)psavings);
+printf("your expenses are %d%% of your income.\n", (int)pexpenses);
+return 0;
+
+}
--- Exspessions/pratice.c
+++ Exspessions/pratice.c
@@ -11,10 +11,10 @@ int main(void){
     printf("%d\n", numbrFour);
     int numbrFive = -2 *(1*4-2/2)+(6+2-3);
     printf("%d\n", numbrFive);
-    int numbrSix = -1*((3-4*7)/5)-2*24/6; 
+    int numbrSix = -1*((3-4*7)/)-2*24/6; 
     printf("%d\n", numbrSix);
     int numbrSeven = (3* pow (5,2)/15)+(5-pow (2,2) ); 
-    printf("%d\n", numbrSeven);
+    printf("%d\n", numbrSeven);5
     int numbrEight = ( pow  (1,4) *pow (2,2)  +pow (3,3)) - (pow (-2,5) /4);
     printf("%d\n", numbrEight);
     int numbrNine = (pow(22/2-2*5,2)+pow (4-6/6,2)); 
