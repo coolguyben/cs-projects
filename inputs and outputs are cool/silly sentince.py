2b72c2db067ee2b171db7cac4937c985d944b93f
coolguyben <benjamin.robison@ucas-edu.net>
2024-10-10 23:34:43 +0000

    yippe

--- inputs and outputs are cool/silly sentince.py
+++ inputs and outputs are cool/silly sentince.py
@@ -1,8 +1,5 @@
-animal= input ("Name an animal")
-place = input ("Name a place ")
-verb = input ("Name a verb")
+animal= input("Name an animal ")
+place = input("Name a place ")
+verb = input("Name a verb ")
 
-print("The", animal)
-print("went to the " ,place)
-print("and it",verb)
-print("Whith thier friend the angry cat")
+print("The", animal, "went to", place, "and he", verb, "with his friend the angry cat")
